d0ea3c1ca25070b35050bb3eca5262308a1a1f54
Vuong Nguyen <vugn@future-demand.com>
2022-09-16 00:26:53 +0200

    Prepare the diagonal matrix

--- src/visualization/report.py
+++ src/visualization/report.py
@@ -15,11 +15,18 @@ class Report:
         for list1, list2 in combinations(data.keys(), 2):
             t, p = ttest_ind(data[list1], data[list2])
             result = (p, "same") if p > p_value else (p, "different")
-
             print(list1, list2, result)
 
     def which_is_better(self):
         data = self.df.to_dict("list")
+
+        matrix_dict = {}
         for l1, l2 in combinations(data.keys(), 2):
             result = VD_A(data[l1], data[l2])
-            print(l1, l2, result)
+            matrix_dict.update({(l1, l2): result[0]})
+            matrix_dict.update({(l2, l1): 1 - result[0]})
+
+        matrix_dict.update({("S.Rand", "S.Rand"): 0.5})
+        matrix_dict.update({("S.OpO", "S.OpO"): 0.5})
+        matrix_dict.update({("M.Rand", "M.Rand"): 0.5})
+        matrix_dict.update({("M.OpO", "M.OpO"): 0.5})
